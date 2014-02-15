5faff0601f073ee74b6cc44ab434438ddb96bc84
soliloquial <weiss.spencer@gmail.com>
2014-02-14 23:48:37 -0500

    Various changes?

--- Jumpshot/src/com/jumpshot/Main.java
+++ Jumpshot/src/com/jumpshot/Main.java
@@ -2,7 +2,10 @@ package com.jumpshot;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class Main extends Activity {
 
@@ -10,7 +13,19 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button submit=(Button)findViewById(R.id.submit);
+        submit.setOnClickListener(onSubmit);
     }
+
+    private View.OnClickListener onSubmit=new View.OnClickListener() {
+
+        @Override
+        public void onClick(View view) {
+            Intent myIntent=new Intent(view.getContext(), CameraActivity.class );
+            startActivityForResult(myIntent,0);
+
+        }
+    };
 
 
     @Override
